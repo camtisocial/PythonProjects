afc6f51c62d4652ce6502d8ab32b8f212d845383
cameron <thompsonca99@gmail.com>
2023-05-24 18:37:34 -0500

    added method for drawing golden triangle

--- FibonacciFun/spiral.py
+++ FibonacciFun/spiral.py
@@ -1,7 +1,8 @@
-import math
 from turtle import Turtle, Screen
+from math import pi, sqrt
 
 #-------- screen setup ---------#
+scale = 50
 screen = Screen()
 screen.setup(width = 1000, height = 1000)
 screen.bgcolor("black")
@@ -11,19 +12,37 @@ turtle = Turtle()
 turtle.radians()
 turtle.showturtle
 turtle.color("white")
-turtle.penup()
-turtle.setpos(-100.0, 0.0)
+
+
+def triangleFib(iterations, turtle):
+    turtle.penup()
+    turtle.setpos(-100.0, 0.0)
+    turtle.pendown()
+
+    goldTriangle(turtle)
+
+    
 
 
 
 
+def goldTriangle(turtle):
+    a = scale*(1+sqrt(5));
+    b = scale*2
+
+    turtle.fd(b);
+    turtle.left(3*pi/5)
+    turtle.fd(a);
+    turtle.left(4*pi/5)
+    turtle.fd(a);
+    turtle.left(pi)
+    turtle.fd(a);
 
 
+def main():
+    triangleFib(3, turtle)
 
-
-
-
-
+main()
 
 
 

eaa8d3d2ace168ab082110b6381a795eae26b9a1
Cameron Thompson <70664942+camtisocial@users.noreply.github.com>
2023-05-24 17:41:45 -0500

    Add files via upload

---
+++ spiral.py
@@ -0,0 +1,32 @@
+import math
+from turtle import Turtle, Screen
+
+#-------- screen setup ---------#
+screen = Screen()
+screen.setup(width = 1000, height = 1000)
+screen.bgcolor("black")
+
+#-------- pen setup ---------#
+turtle = Turtle()
+turtle.radians()
+turtle.showturtle
+turtle.color("white")
+turtle.penup()
+turtle.setpos(-100.0, 0.0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()

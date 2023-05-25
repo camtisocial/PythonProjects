f40f9e48b9b57abff68521143c75c52aa8292f1d
cameron <thompsonca99@gmail.com>
2023-05-24 22:22:43 -0500

    added addGnomon function

--- FibonacciFun/spiral.py
+++ FibonacciFun/spiral.py
@@ -1,8 +1,17 @@
 from turtle import Turtle, Screen
 from math import pi, sqrt
 
+#-------- variables---------#
+
+#for iso triangles
+scale = 20
+a = 1+sqrt(5)
+b = 2
+gratio = a/b
+baseGnomonScale = a*scale
+
+
 #-------- screen setup ---------#
-scale = 50
 screen = Screen()
 screen.setup(width = 1000, height = 1000)
 screen.bgcolor("black")
@@ -15,32 +24,41 @@ turtle.color("white")
 
 
 def triangleFib(iterations, turtle):
+    #positioning turtle
     turtle.penup()
-    turtle.setpos(-100.0, 0.0)
+    turtle.setpos(-120.0, 100.0)
     turtle.pendown()
 
+    #drawing initial triangle
     goldTriangle(turtle)
 
+    #drawing gnomons
+    for _ in range (iterations):
+        goldGnomon(turtle)
+
     
-
-
-
-
 def goldTriangle(turtle):
-    a = scale*(1+sqrt(5));
-    b = scale*2
-
-    turtle.fd(b);
+    turtle.fd(b*scale)
     turtle.left(3*pi/5)
-    turtle.fd(a);
+    turtle.fd(a*scale)
     turtle.left(4*pi/5)
-    turtle.fd(a);
+    turtle.fd(a*scale)
     turtle.left(pi)
-    turtle.fd(a);
+    turtle.fd(a*scale)
+    turtle.left(pi/5)
 
+def goldGnomon(turtle):
+    global baseGnomonScale
+    baseGnomonScale*=gratio
+    turtle.right(4*pi/5)
+    turtle.fd(baseGnomonScale)
+    turtle.right(4*pi/5)
+    turtle.fd(baseGnomonScale/gratio)
+    turtle.right(pi)
+    turtle.fd(baseGnomonScale/gratio)
 
 def main():
-    triangleFib(3, turtle)
+    triangleFib(5, turtle)
 
 main()
 
